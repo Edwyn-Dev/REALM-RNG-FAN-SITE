8a8da410f2af639530e402c69b91d555fddb4f74
Edwyn-Dev <playstation.edwyn@gmail.com>
2024-06-11 22:09:09 +0200

    added base hp

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
             const stats = $('<div class="race-stats"></div>');
 
             stats.append(`<div class="race-stat"><span class="stat-label">❤️ ${formatNumber(details.health)} HP</div></span>`);
-            stats.append(`<div class="race-stat"><span class="stat-label">🎲 1 in ${formatNumber(details.probability)}</div></span>`);
+            stats.append(`<div class="race-stat"><span class="stat-label">🎲 1 in ${formatNumber(100+details.probability)}</div></span>`);
             stats.append(`<div class="race-stat"><span class="stat-label">${(details.rarity).toUpperCase()}</div></span>`);
 
             content.append(title, description, stats);
